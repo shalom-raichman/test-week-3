8a0384c2669e31f3e2ce0c54b9c46764f2ac0194
Shalom Raichman <shalomr1770@gmail.com>
2024-09-19 13:46:28 +0300

    lunch break

--- app.ts
+++ app.ts
@@ -1,6 +1,42 @@
 const rangeDispInp: NodeListOf<HTMLLabelElement> = document.querySelectorAll(".range-inp")
 const rangeDispLbl: NodeListOf<HTMLInputElement> = document.querySelectorAll(".range-lbl")
 
+// return action element
+const createAddPlayerElement = (): HTMLElement => {
+    const box: HTMLElement = document.createElement("td")
+    const wraper = document.createElement("div")
+    wraper.classList.add("actions-div")
+    box.appendChild(wraper)
+    return box
+}
+
+// return row element
+const createRow = (player: Player): HTMLElement => {
+    // creting row content
+    const row: HTMLElement = document.createElement("tr")
+    const name: HTMLElement = document.createElement("td")
+    name.textContent = player.playerName
+    const position: HTMLElement = document.createElement("td")
+    position.textContent = player.position
+    const points: HTMLElement = document.createElement("td")
+    points.textContent = player.points.toString()
+    const twoPercent: HTMLElement = document.createElement("td")
+    twoPercent.textContent = player.twoPercent.toString()
+    const threePercent:HTMLElement = document.createElement("td")
+    threePercent.textContent = player.threePercent.toString()
+    const action:HTMLElement = createAddPlayerElement()
+
+    // inserting the elements to the row
+    row.appendChild(name)
+    row.appendChild(position)
+    row.appendChild(points)
+    row.appendChild(twoPercent)
+    row.appendChild(threePercent)
+    row.appendChild(action)
+
+    return row
+}
+
 
 
 
@@ -17,3 +53,16 @@ const rangeDisply = (): void => {
 }
 
 rangeDisply()
+
+interface Player {
+    _id: string,
+    playerName: string,
+    age: number,
+    position: string,
+    twoPercent: number,
+    threePercent: number,
+    games: number,
+    team: string,
+    season: number[],
+    points: number,
+}
--- js/app.js
+++ js/app.js
@@ -1,6 +1,38 @@
 "use strict";
 const rangeDispInp = document.querySelectorAll(".range-inp");
 const rangeDispLbl = document.querySelectorAll(".range-lbl");
+// return action element
+const createAddPlayerElement = () => {
+    const box = document.createElement("td");
+    const wraper = document.createElement("div");
+    wraper.classList.add("actions-div");
+    box.appendChild(wraper);
+    return box;
+};
+// return row element
+const createRow = (player) => {
+    // creting row content
+    const row = document.createElement("tr");
+    const name = document.createElement("td");
+    name.textContent = player.playerName;
+    const position = document.createElement("td");
+    position.textContent = player.position;
+    const points = document.createElement("td");
+    points.textContent = player.points.toString();
+    const twoPercent = document.createElement("td");
+    twoPercent.textContent = player.twoPercent.toString();
+    const threePercent = document.createElement("td");
+    threePercent.textContent = player.threePercent.toString();
+    const action = createAddPlayerElement();
+    // inserting the elements to the row
+    row.appendChild(name);
+    row.appendChild(position);
+    row.appendChild(points);
+    row.appendChild(twoPercent);
+    row.appendChild(threePercent);
+    row.appendChild(action);
+    return row;
+};
 const rangeDisply = () => {
     for (let i = 0; i < rangeDispInp.length; i++) {
         const inp = rangeDispInp[i];
