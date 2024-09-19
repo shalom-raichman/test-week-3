e3d1e6b24dbb6946cff65c6299948f3794908802
Shalom Raichman <shalomr1770@gmail.com>
2024-09-19 17:37:42 +0300

    to the finish line

--- app.ts
+++ app.ts
@@ -1,11 +1,117 @@
+const BASE_URL: string = "https://nbaserver-q21u.onrender.com/"
+const searchBtn: HTMLDivElement = document.querySelector(".subm")!
+const table: HTMLElement = document.querySelector(".tbody")!
+const playrsRows: HTMLElement = document.querySelector(".data")!
+
+
 const rangeDispInp: NodeListOf<HTMLLabelElement> = document.querySelectorAll(".range-inp")
 const rangeDispLbl: NodeListOf<HTMLInputElement> = document.querySelectorAll(".range-lbl")
 
+const PG: HTMLDivElement = document.querySelector(".PG")! 
+const SG: HTMLDivElement = document.querySelector(".SG")! 
+const SF: HTMLDivElement = document.querySelector(".SF")! 
+const PF: HTMLDivElement = document.querySelector(".PF")! 
+const C: HTMLDivElement = document.querySelector(".C")! 
+
+let fantasyTesm: Player[] = []
+
+const playersReq = (): PlayerReq => {
+    const playerReq: PlayerReq = {
+        position: (rangeDispInp[0] as unknown as HTMLInputElement).value,
+        points: Number((rangeDispInp[1] as unknown as HTMLInputElement).value),
+        twoPercent: Number((rangeDispInp[2] as unknown as HTMLInputElement).value),
+        threePercent: Number((rangeDispInp[3] as unknown as HTMLInputElement).value)
+    }    
+    return playerReq
+}
+
+const getPlayers = async (): Promise<Player[] | undefined> => {    
+    try {
+        const res = await fetch(`${BASE_URL}api/filter`,{
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(playersReq())
+        })
+        if(!res.ok){
+            throw new Error(res.statusText);
+        }
+        const players: Player[] = await res.json()
+        return players
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const populateTeam = (player: Player): void => {
+    const playerName: HTMLParagraphElement = document.createElement("p")
+    playerName.textContent = player.playerName
+    const player3p: HTMLParagraphElement = document.createElement("p")
+    player3p.textContent = player.threePercent.toString()
+    const player2p: HTMLParagraphElement = document.createElement("p")
+    player2p.textContent = player.twoPercent.toString()
+    const playerPoints: HTMLParagraphElement = document.createElement("p")
+    playerPoints.textContent = player.position
+
+    switch (player.position) {
+        case "PG":
+            PG.appendChild(playerName)
+            PG.appendChild(player3p)
+            PG.appendChild(player2p)
+            PG.appendChild(playerPoints)
+            break;
+        case "SG":
+            SG.appendChild(playerName)
+            SG.appendChild(player3p)
+            SG.appendChild(player2p)
+            SG.appendChild(playerPoints)
+            break;
+        case "SF":
+            SF.appendChild(playerName)
+            SF.appendChild(player3p)
+            SF.appendChild(player2p)
+            SF.appendChild(playerPoints)
+            break;
+        case "PF":
+            PF.appendChild(playerName)
+            PF.appendChild(player3p)
+            PF.appendChild(player2p)
+            PF.appendChild(playerPoints)
+            break;
+        case "C":
+            C.appendChild(playerName)
+            C.appendChild(player3p)
+            C.appendChild(player2p)
+            C.appendChild(playerPoints)
+            break;
+    
+        default:
+            break;
+    }
+
+}
+
+
+const addPlayer = (player: Player) => {
+    const toAdd = fantasyTesm.find(p => p.position == player.position)
+    if(toAdd) return
+    fantasyTesm.push(player)
+    for (const player of fantasyTesm) {
+        populateTeam(player)
+    }
+}
+
 // return action element
-const createAddPlayerElement = (): HTMLElement => {
+const createAddPlayerElement = (player: Player): HTMLElement => {
     const box: HTMLElement = document.createElement("td")
-    const wraper = document.createElement("div")
+    const wraper: HTMLDivElement = document.createElement("div")
     wraper.classList.add("actions-div")
+    const addplayer: HTMLDivElement = document.createElement("div")
+    addplayer.classList.add("action")
+    addplayer.textContent = "Add Player"
+    addplayer.addEventListener("click", e => addPlayer(player))
+    wraper.appendChild(addplayer)
     box.appendChild(wraper)
     return box
 }
@@ -13,44 +119,66 @@ const createAddPlayerElement = (): HTMLElement => {
 // return row element
 const createRow = (player: Player): HTMLElement => {
     // creting row content
+    // box
     const row: HTMLElement = document.createElement("tr")
+    // player name
     const name: HTMLElement = document.createElement("td")
     name.textContent = player.playerName
+    // player position
     const position: HTMLElement = document.createElement("td")
     position.textContent = player.position
+    // player 
     const points: HTMLElement = document.createElement("td")
     points.textContent = player.points.toString()
+    // player 2 percent
     const twoPercent: HTMLElement = document.createElement("td")
     twoPercent.textContent = player.twoPercent.toString()
+    // player 3 percent
     const threePercent:HTMLElement = document.createElement("td")
     threePercent.textContent = player.threePercent.toString()
-    const action:HTMLElement = createAddPlayerElement()
-
+    // add player action
+    const addPlayer :HTMLElement = createAddPlayerElement(player)
+    
     // inserting the elements to the row
     row.appendChild(name)
     row.appendChild(position)
     row.appendChild(points)
     row.appendChild(twoPercent)
     row.appendChild(threePercent)
-    row.appendChild(action)
-
+    row.appendChild(addPlayer)
     return row
 }
 
-
-
-
 const rangeDisply = (): void => {
     for (let i:number = 0; i < rangeDispInp.length; i++) {
-        const inp = rangeDispInp[i];
+        const inp = rangeDispInp[i + 1];
         const lbl = rangeDispLbl[i];
         if(i == 0){
             inp.addEventListener("click", () => {lbl.textContent = (inp as unknown as HTMLInputElement).value})
         }else{
-            inp.addEventListener("click", () => {lbl.textContent = (inp as unknown as HTMLInputElement).value + "%"})
+            inp.addEventListener("click", () => {lbl.textContent = (inp as unknown as HTMLInputElement).value.toString() + "%"})
         }
     }
 }
+
+const populatePlayers = async (): Promise<void> => {
+    const players: Player[] | undefined = await getPlayers()
+    if(players?.length == 0){
+        alert("There is no players with thees expectation")
+        console.log(table);
+
+    }
+    
+    table.innerHTML = ""
+    for (const player of (players as Player[])) {
+        table.appendChild(createRow(player))
+    }
+    console.log(table);
+
+}
+
+
+searchBtn.addEventListener("click", populatePlayers)
 
 rangeDisply()
 
@@ -58,7 +186,7 @@ interface Player {
     _id: string,
     playerName: string,
     age: number,
-    position: string,
+    position: positions,
     twoPercent: number,
     threePercent: number,
     games: number,
@@ -66,3 +194,15 @@ interface Player {
     season: number[],
     points: number,
 }
+interface PlayerReq {
+    position: string,
+    twoPercent: number,
+    threePercent: number,
+    points: number,
+}
+
+// type positions = "PG"|"SG"|"SF"|"PF"|"C"
+
+// type Team =  {
+//     [key in positions]: Player;
+// }
--- js/app.js
+++ js/app.js
@@ -1,48 +1,177 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const BASE_URL = "https://nbaserver-q21u.onrender.com/";
+const searchBtn = document.querySelector(".subm");
+const table = document.querySelector(".tbody");
+const playrsRows = document.querySelector(".data");
 const rangeDispInp = document.querySelectorAll(".range-inp");
 const rangeDispLbl = document.querySelectorAll(".range-lbl");
+const PG = document.querySelector(".PG");
+const SG = document.querySelector(".SG");
+const SF = document.querySelector(".SF");
+const PF = document.querySelector(".PF");
+const C = document.querySelector(".C");
+let fantasyTesm = [];
+const playersReq = () => {
+    const playerReq = {
+        position: rangeDispInp[0].value,
+        points: Number(rangeDispInp[1].value),
+        twoPercent: Number(rangeDispInp[2].value),
+        threePercent: Number(rangeDispInp[3].value)
+    };
+    return playerReq;
+};
+const getPlayers = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const res = yield fetch(`${BASE_URL}api/filter`, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(playersReq())
+        });
+        if (!res.ok) {
+            throw new Error(res.statusText);
+        }
+        const players = yield res.json();
+        return players;
+    }
+    catch (error) {
+        console.log(error);
+    }
+});
+const populateTeam = (player) => {
+    const playerName = document.createElement("p");
+    playerName.textContent = player.playerName;
+    const player3p = document.createElement("p");
+    player3p.textContent = player.threePercent.toString();
+    const player2p = document.createElement("p");
+    player2p.textContent = player.twoPercent.toString();
+    const playerPoints = document.createElement("p");
+    playerPoints.textContent = player.position;
+    switch (player.position) {
+        case "PG":
+            PG.appendChild(playerName);
+            PG.appendChild(player3p);
+            PG.appendChild(player2p);
+            PG.appendChild(playerPoints);
+            break;
+        case "SG":
+            SG.appendChild(playerName);
+            SG.appendChild(player3p);
+            SG.appendChild(player2p);
+            SG.appendChild(playerPoints);
+            break;
+        case "SF":
+            SF.appendChild(playerName);
+            SF.appendChild(player3p);
+            SF.appendChild(player2p);
+            SF.appendChild(playerPoints);
+            break;
+        case "PF":
+            PF.appendChild(playerName);
+            PF.appendChild(player3p);
+            PF.appendChild(player2p);
+            PF.appendChild(playerPoints);
+            break;
+        case "C":
+            C.appendChild(playerName);
+            C.appendChild(player3p);
+            C.appendChild(player2p);
+            C.appendChild(playerPoints);
+            break;
+        default:
+            break;
+    }
+};
+const addPlayer = (player) => {
+    const toAdd = fantasyTesm.find(p => p.position == player.position);
+    if (toAdd)
+        return;
+    fantasyTesm.push(player);
+    for (const player of fantasyTesm) {
+        populateTeam(player);
+    }
+};
 // return action element
-const createAddPlayerElement = () => {
+const createAddPlayerElement = (player) => {
     const box = document.createElement("td");
     const wraper = document.createElement("div");
     wraper.classList.add("actions-div");
+    const addplayer = document.createElement("div");
+    addplayer.classList.add("action");
+    addplayer.textContent = "Add Player";
+    addplayer.addEventListener("click", e => addPlayer(player));
+    wraper.appendChild(addplayer);
     box.appendChild(wraper);
     return box;
 };
 // return row element
 const createRow = (player) => {
     // creting row content
+    // box
     const row = document.createElement("tr");
+    // player name
     const name = document.createElement("td");
     name.textContent = player.playerName;
+    // player position
     const position = document.createElement("td");
     position.textContent = player.position;
+    // player 
     const points = document.createElement("td");
     points.textContent = player.points.toString();
+    // player 2 percent
     const twoPercent = document.createElement("td");
     twoPercent.textContent = player.twoPercent.toString();
+    // player 3 percent
     const threePercent = document.createElement("td");
     threePercent.textContent = player.threePercent.toString();
-    const action = createAddPlayerElement();
+    // add player action
+    const addPlayer = createAddPlayerElement(player);
     // inserting the elements to the row
     row.appendChild(name);
     row.appendChild(position);
     row.appendChild(points);
     row.appendChild(twoPercent);
     row.appendChild(threePercent);
-    row.appendChild(action);
+    row.appendChild(addPlayer);
     return row;
 };
 const rangeDisply = () => {
     for (let i = 0; i < rangeDispInp.length; i++) {
-        const inp = rangeDispInp[i];
+        const inp = rangeDispInp[i + 1];
         const lbl = rangeDispLbl[i];
         if (i == 0) {
             inp.addEventListener("click", () => { lbl.textContent = inp.value; });
         }
         else {
-            inp.addEventListener("click", () => { lbl.textContent = inp.value + "%"; });
+            inp.addEventListener("click", () => { lbl.textContent = inp.value.toString() + "%"; });
         }
     }
 };
+const populatePlayers = () => __awaiter(void 0, void 0, void 0, function* () {
+    const players = yield getPlayers();
+    if ((players === null || players === void 0 ? void 0 : players.length) == 0) {
+        alert("There is no players with thees expectation");
+        console.log(table);
+    }
+    table.innerHTML = "";
+    for (const player of players) {
+        table.appendChild(createRow(player));
+    }
+    console.log(table);
+});
+searchBtn.addEventListener("click", populatePlayers);
 rangeDisply();
+// type positions = "PG"|"SG"|"SF"|"PF"|"C"
+// type Team =  {
+//     [key in positions]: Player;
+// }
